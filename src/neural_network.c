d3d37c809e4a57032766e2bfc4b2711034ed0302
Maike Santana <128054364+MaikeRob@users.noreply.github.com>
2024-12-10 13:05:25 +0000

    começando desenvolvimento de predições

--- src/neural_network.c
+++ src/neural_network.c
@@ -76,3 +76,8 @@ void add_layer(model_t *model, unsigned int num_neurons, const char *activation)
     new_layer->activation = activation;
     model->num_layers++;
 }
+
+void predict(model_t model, double *input) {
+
+    
+}
